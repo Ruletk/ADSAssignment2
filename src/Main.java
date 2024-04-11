d9ab68a26ccb408dc68614fdbb2cf92abd450ed9
Ruletk <damir.timergazin@gmail.com>
2024-04-11 15:48:13 +0500

    Created MyList interface for further MyArrayList and MyLinkedList classes. Provides some methods for manipulating with an array.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import list.MyList;
+
 public class Main {
     public static void main(String[] args) {
-
+        MyList<?> myList;
     }
 }
--- src/list/MyList.java
+++ src/list/MyList.java
@@ -1,5 +1,22 @@
 package list;
 
-public interface MyList {
-
+public interface MyList<T> {
+    void add(T item);
+    void add(T item, int index);
+    void addFirst(T item);
+    void addLast(T item);
+    void set(T item, int index);
+    T get(int index);
+    T getFirst();
+    T getLast();
+    void remove(int index);
+    void removeFirst();
+    void removeLast();
+    void sort();
+    int indexOf(T item);
+    int lastIndexOf(T item);
+    boolean exists(T item);
+    T[] toArray();
+    void clear();
+    int size();
 }
