badc0fa00cf436c754a25624b620bdac9348fc34
Ruletk <damir.timergazin@gmail.com>
2024-04-20 12:09:43 +0500

    Created MyArrayList, MyList implementation.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import list.MyArrayList;
 import list.MyList;
+
 
 public class Main {
     public static void main(String[] args) {
-        MyList<?> myList;
+        MyList<Integer> myArrayList = new MyArrayList<>();
+        myArrayList.add(1, 0);
+        myArrayList.add(2, 1);
+        myArrayList.add(5);
+        System.out.println(myArrayList);
+
+        myArrayList.remove(0);
+        System.out.println(myArrayList);
+
+        System.out.println(myArrayList.get(2));
     }
+
 }
